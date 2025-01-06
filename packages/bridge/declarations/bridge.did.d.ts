09d261ad5af33c75bfc2a3398d51df7bfb1f0352
Kristofer <kristofer@fmckl.se>
2025-01-06 15:10:57 +0100

    Rebuilding bridge interface

--- packages/bridge/declarations/bridge.did.d.ts
+++ packages/bridge/declarations/bridge.did.d.ts
@@ -4,23 +4,46 @@ import type { IDL } from '@dfinity/candid';
 
 export interface EthPoolLiquidityPositionDto {
   'last_claimed_fee_per_token' : string,
+  'timestamp' : bigint,
+  'tx_hash' : string,
   'amount' : string,
 }
+export type Event = { 'Init' : InitArgs } |
+  { 'RegisterEthAddress' : [Principal, Uint8Array | number[]] } |
+  { 'CreateUser' : Principal } |
+  { 'PostUpgrade' : InitArgs } |
+  {
+    'EThPoolCreatePosition' : [Principal, string, Uint8Array | number[], bigint]
+  };
 export interface HttpError {
   'code' : number,
   'message' : string,
   'details' : [] | [string],
 }
-export interface InitArgs { 'eth_min_confirmations' : bigint }
-export type Result = { 'Ok' : EthPoolLiquidityPositionDto } |
+export interface InitArgs {
+  'ecdsa_key_id' : string,
+  'evm_rpc_url' : string,
+  'siwe_provider_canister' : string,
+  'eth_min_confirmations' : bigint,
+}
+export type Result = { 'Ok' : string } |
   { 'Err' : HttpError };
-export type Result_1 = { 'Ok' : User } |
+export type Result_1 = { 'Ok' : EthPoolLiquidityPositionDto } |
   { 'Err' : HttpError };
-export interface User { 'eth_address' : Uint8Array | number[] }
+export type Result_2 = { 'Ok' : Array<Event> } |
+  { 'Err' : HttpError };
+export type Result_3 = { 'Ok' : UserDto } |
+  { 'Err' : HttpError };
+export interface UserDto {
+  'principal' : Principal,
+  'eth_address' : [] | [string],
+}
 export interface _SERVICE {
-  'eth_pool_create_position' : ActorMethod<[string], Result>,
-  'user_create' : ActorMethod<[], Result_1>,
-  'user_register_eth_address' : ActorMethod<[], Result_1>,
+  'eth_pool_address' : ActorMethod<[], Result>,
+  'eth_pool_create_position' : ActorMethod<[string], Result_1>,
+  'event_log' : ActorMethod<[], Result_2>,
+  'user_create' : ActorMethod<[], Result_3>,
+  'user_register_eth_address' : ActorMethod<[], Result_3>,
 }
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
--- packages/bridge/declarations/bridge.did.js
+++ packages/bridge/declarations/bridge.did.js
@@ -1,27 +1,58 @@
 export const idlFactory = ({ IDL }) => {
-  const InitArgs = IDL.Record({ 'eth_min_confirmations' : IDL.Nat64 });
-  const EthPoolLiquidityPositionDto = IDL.Record({
-    'last_claimed_fee_per_token' : IDL.Text,
-    'amount' : IDL.Text,
+  const InitArgs = IDL.Record({
+    'ecdsa_key_id' : IDL.Text,
+    'evm_rpc_url' : IDL.Text,
+    'siwe_provider_canister' : IDL.Text,
+    'eth_min_confirmations' : IDL.Nat64,
   });
   const HttpError = IDL.Record({
     'code' : IDL.Nat16,
     'message' : IDL.Text,
     'details' : IDL.Opt(IDL.Text),
   });
-  const Result = IDL.Variant({
+  const Result = IDL.Variant({ 'Ok' : IDL.Text, 'Err' : HttpError });
+  const EthPoolLiquidityPositionDto = IDL.Record({
+    'last_claimed_fee_per_token' : IDL.Text,
+    'timestamp' : IDL.Nat64,
+    'tx_hash' : IDL.Text,
+    'amount' : IDL.Text,
+  });
+  const Result_1 = IDL.Variant({
     'Ok' : EthPoolLiquidityPositionDto,
     'Err' : HttpError,
   });
-  const User = IDL.Record({ 'eth_address' : IDL.Vec(IDL.Nat8) });
-  const Result_1 = IDL.Variant({ 'Ok' : User, 'Err' : HttpError });
+  const Event = IDL.Variant({
+    'Init' : InitArgs,
+    'RegisterEthAddress' : IDL.Tuple(IDL.Principal, IDL.Vec(IDL.Nat8)),
+    'CreateUser' : IDL.Principal,
+    'PostUpgrade' : InitArgs,
+    'EThPoolCreatePosition' : IDL.Tuple(
+      IDL.Principal,
+      IDL.Text,
+      IDL.Vec(IDL.Nat8),
+      IDL.Nat64,
+    ),
+  });
+  const Result_2 = IDL.Variant({ 'Ok' : IDL.Vec(Event), 'Err' : HttpError });
+  const UserDto = IDL.Record({
+    'principal' : IDL.Principal,
+    'eth_address' : IDL.Opt(IDL.Text),
+  });
+  const Result_3 = IDL.Variant({ 'Ok' : UserDto, 'Err' : HttpError });
   return IDL.Service({
-    'eth_pool_create_position' : IDL.Func([IDL.Text], [Result], ['query']),
-    'user_create' : IDL.Func([], [Result_1], []),
-    'user_register_eth_address' : IDL.Func([], [Result_1], []),
+    'eth_pool_address' : IDL.Func([], [Result], ['query']),
+    'eth_pool_create_position' : IDL.Func([IDL.Text], [Result_1], []),
+    'event_log' : IDL.Func([], [Result_2], ['query']),
+    'user_create' : IDL.Func([], [Result_3], []),
+    'user_register_eth_address' : IDL.Func([], [Result_3], []),
   });
 };
 export const init = ({ IDL }) => {
-  const InitArgs = IDL.Record({ 'eth_min_confirmations' : IDL.Nat64 });
+  const InitArgs = IDL.Record({
+    'ecdsa_key_id' : IDL.Text,
+    'evm_rpc_url' : IDL.Text,
+    'siwe_provider_canister' : IDL.Text,
+    'eth_min_confirmations' : IDL.Nat64,
+  });
   return [InitArgs];
 };
